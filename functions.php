a1f350454c291ce713e2cf0233510f9f051bcb6a
McKenna DeBandi <71356517+mck-debandi@users.noreply.github.com>
2026-07-16 16:42:14 -0400

    Implemented SCSS architecture

--- functions.php
+++ functions.php
@@ -1,22 +1,15 @@
 <?php 
 
 /** 
- * Enqueues the theme stylesheet on the front.
+ * Enqueues the minified compiled stylesheet
  */
 function blackducktheme_enqueue_styles() {
-	$suffix = SCRIPT_DEBUG ? '' : '.min';
-	$src    = 'style' . $suffix . '.css';
 
 	wp_enqueue_style(
 		'blackducktheme-style',
-		get_parent_theme_file_uri( $src ),
-		array(),
+		get_template_directory_uri() . '/assets/css/style.min.css',
+		[],
 		wp_get_theme()->get( 'Version' )
-	);
-	wp_style_add_data(
-		'blackducktheme-style',
-		'path',
-		get_parent_theme_file_path( $src )
 	);
 }
 add_action( 'wp_enqueue_scripts', 'blackducktheme_enqueue_styles' );
